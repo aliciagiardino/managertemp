d01493d3c8f9d39722a82de291f357921cbb2063
Leonardo Labolida <labolida.leonardo@gmail.com>
2022-03-23 13:03:23 +0100

    First commit

---
+++ SuperPrograma.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Super programa";
+
+$x=getInt10();
+
+$x = $x + 10;
+
+echo "Deveria de ser 20:" .$x;
+
+
+function getInt10(){
+	return 10;
+}
+
+
+// modificacion comentario - usando nuevo usuario git
+?>
